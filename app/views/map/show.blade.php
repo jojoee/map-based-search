915a7597d405c549afd967c374a74f475486bf92
Nathachai Thongniran <inid3a@gmail.com>
2015-06-15 08:10:36 +0700

    add / update placeholder text for all input text fields
    - add / update placeholder text for all input text fields

--- app/views/map/show.blade.php
+++ app/views/map/show.blade.php
@@ -8,15 +8,15 @@
 	</div>
 </div>
 <div id="map-config"><!-- #map-config -->
-	<label for="zoom-level">Zoom Level: </label><input type="text" name="zoom-level" id="zoom-level">
-	<label for="lat">Lat: </label><input type="text" name="lat" id="lat">
-	<label for="lng">Lng: </label><input type="text" name="lng" id="lng">
+	<label for="zoom-level">Zoom Level: </label><input type="text" name="zoom-level" id="zoom-level" placeholder="Zoom level">
+	<label for="lat">Lat: </label><input type="text" name="lat" id="lat" placeholder="Latitude">
+	<label for="lng">Lng: </label><input type="text" name="lng" id="lng" placeholder="Longitude">
 	<div class="remove-all-markers btn">Remove All Markers</div>
 	<div class="add-text-overlay btn">Add text overlay</div>
 	<div class="update-text-overlay btn">Update text overlay</div>
 	<div class="remove-text-overlay btn">Remove text overlay</div>
 	{{ Form::open(array('id' => 'city-form')) }}
-		{{ Form::text('city', Input::old('city'), array('placeholder' => 'City', 'id' => 'city', 'required' => 'required')) }}
+		{{ Form::text('city', Input::old('city'), array('placeholder' => 'City name', 'id' => 'city', 'required' => 'required')) }}
 		{{ Form::submit('Search') }}
 	{{ Form::close() }}
 </div>
