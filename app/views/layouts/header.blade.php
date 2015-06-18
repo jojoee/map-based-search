31a03db1033bfa0ad55dac9ded4852b3809572f8
Nathachai Thongniran <inid3a@gmail.com>
2015-06-18 21:19:04 +0700

    update main font
    - use sans-serif instead of Open Sans

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -7,7 +7,6 @@
 <meta name="description" content="simple responsive website that allows the user to search for a city  and displays tweets that mention the city on a map.">
 <meta name="keywords" content="Map Based Search, Twitter">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-{{ HTML::style('http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700') }}
 {{ HTML::style('css/reset.min.css') }}
 {{ HTML::style('https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css') }}
 {{ HTML::style('css/style.css') }}
