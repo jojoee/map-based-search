6545d546c6e8f52b3ca9d4ff18acc48f7aa867d5
Nathachai Thongniran <inid3a@gmail.com>
2016-02-21 08:45:25 +0700

    Add Google map API key

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 </div><!-- .container -->
 <div class="hidden-script">
 	{{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js') }}
-	{{ HTML::script('https://maps.googleapis.com/maps/api/js') }}
+	{{ HTML::script('https://maps.googleapis.com/maps/api/js?key=AIzaSyB0vTTx1dldiq2AvGO63yFIq-O5LB3dxWU') }}
 	{{ HTML::script('js/utilities.js') }}
 	{{ HTML::script('js/variables.js') }}
 	{{ HTML::script('js/app.js') }}
