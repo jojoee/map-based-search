dc5ac7ccc19ad1f3d0760c012601647e99a1b02a
Nathachai Thongniran <inid3a@gmail.com>
2016-08-02 02:05:33 +0700

    Update

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'mbs.jojoee.com_db',
+			'database'  => 'mbs_db',
 			'username'  => 'root',
 			'password'  => 'myServer',
 			'charset'   => 'utf8',
